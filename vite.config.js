3d5a36631eb937b8c505338b48357fc3e5cfef3c
andrronov <andrew270504cska@gmail.com>
2023-11-17 15:17:25 +0300

    DB connected, all works

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch');
+  },
 })
+
+// module.exports = {
+//   chainWebpack: (config) => {
+//     config.plugins.delete('prefetch');
+//   },
+// };
