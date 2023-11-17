93d1f6143656e4c7523bd25ef9785b49184f4935
andrronov <andrew270504cska@gmail.com>
2023-11-17 20:37:26 +0300

    router works but ADD / COMPONENT

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,3 @@
 <template>
-  <MainComponent @testData="takeTest" v-if="!testOpened" />
-  <TestComponent :testPath="testPath" v-if="testOpened" />
+  <router-view />
 </template>
-
-<script>
-import MainComponent from './components/MainComponent.vue'
-import TestComponent from "./components/testComponent.vue"
-export default {
-  name: "App",
-  components: {
-    MainComponent,
-    TestComponent,
-  },
-  data(){
-    return{
-      testOpened: false,
-      testPath: "./",
-    }
-  },
-  methods: {
-    takeTest(data){
-      this.testPath = data
-      this.testOpened = true
-    }
-  }
-};
-</script>
-
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap');
-body{
-  font-family: 'Lato', sans-serif;
-}
-</style>
