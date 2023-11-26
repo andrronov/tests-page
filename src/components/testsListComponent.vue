8041c9d595f47235771b5b8e263c28f7914083a2
andrronov <andrew270504cska@gmail.com>
2023-11-26 16:06:34 +0300

    searching for categories done + source file

--- src/components/testsListComponent.vue
+++ src/components/testsListComponent.vue
@@ -2,42 +2,63 @@
   <header class="header">
     <v-container class="d-flex flex-column align-center">
       <v-container class="d-flex flex-row align-center">
-      <v-autocomplete
-        class="w-100 header_search"
-        style="color: white"
-        variant="outlined"
-        label="Найти тест"
-        :items="allTests?.testsList"
-        @keydown.enter="findSearchedTest"
-        v-model="searchValue"
-      ></v-autocomplete>
-      <v-btn @click="findSearchedTest" icon="mdi-magnify"></v-btn>
+        <v-autocomplete
+          class="header_search"
+          style="color: white"
+          variant="outlined"
+          label="Найти тест"
+          :items="allTests?.testsList"
+          @keydown.enter="findSearchedTest"
+          v-model="searchValue"
+        ></v-autocomplete>
+        <v-btn @click="findSearchedTest" icon="mdi-magnify"></v-btn>
       </v-container>
+      <v-combobox
+      class="w-100 header_search"
+        style="color: white"
+        clearable
+        chips
+        multiple
+        label="Категории"
+        :items="testCategory"
+        variant="outlined"
+        v-model="searchCategories"
+      ></v-combobox>
     </v-container>
   </header>
 
   <div class="wrapper">
     <h2 class="title">Выберите тест, который хотите пройти:</h2>
     <v-container v-if="allTests?.newTestArray">
-      <div class="title_button" style="display: flex; flex-direction: column; gap:10px">
+      <div
+        class="title_button"
+        style="display: flex; flex-direction: column; gap: 10px"
+      >
         <h4 class="title">Найденные тесты</h4>
-        <h3 style="align-self:center; margin: 25px 0px" v-if="allTests.newTestArray == 0">Ничего не найдено</h3>
-      <v-btn @click="allTests.newTestArray = 0" variant="outlined">Отменить поиск</v-btn>
+        <h3
+          style="align-self: center; margin: 25px 0px"
+          v-if="allTests.newTestArray == 0"
+        >
+          Ничего не найдено
+        </h3>
+        <v-btn @click="allTests.newTestArray = 0" variant="outlined"
+          >Отменить поиск</v-btn
+        >
       </div>
       <v-col v-for="(test, i) in allTests.newTestArray" :key="i">
         <v-card color="indigo-darken-3" variant="outlined">
           <v-card-item>
             <div>
               <div class="text-h6 mb-1">{{ test.title }}</div>
-              <div class="text-caption">{{ test.description }}</div>
+              <div class="text-body-1 mb-1">{{ test.description }}</div>
+              <div class="text-body-2">Категория: {{ test.category }}</div>
+              <div class="text-body-2">Автор: {{ test.author }}</div>
             </div>
           </v-card-item>
 
           <v-card-actions>
             <router-link :to="{ name: 'test', params: { id: test.id } }">
-              <v-btn variant="outlined" color="indigo-darken-3">
-                Пройти
-              </v-btn>
+              <v-btn variant="outlined" color="indigo-darken-3"> Пройти </v-btn>
             </router-link>
           </v-card-actions>
         </v-card>
@@ -45,25 +66,24 @@
     </v-container>
     <v-container v-if="allTests">
       <h4 class="title">Все тесты</h4>
-        <v-col v-for="(test, i) in allTests.testsList" :key="i">
-          <v-card color="indigo-darken-3" variant="outlined">
-            <v-card-item>
-              <div>
-                <div class="text-h6 mb-1">{{ test.title }}</div>
-                <div class="text-caption">{{ test.description }}</div>
-                <div class="text-caption">Категория: {{ test.category }}</div>
-              </div>
-            </v-card-item>
+      <v-col v-for="(test, i) in allTests.testsList" :key="i">
+        <v-card color="indigo-darken-3" variant="outlined">
+          <v-card-item>
+            <div>
+              <div class="text-h6 mb-1">{{ test.title }}</div>
+              <div class="text-body-1 mb-1">{{ test.description }}</div>
+              <div class="text-body-2">Категория: {{ test.category }}</div>
+              <div class="text-body-2">Автор: {{ test.author }}</div>
+            </div>
+          </v-card-item>
 
-            <v-card-actions>
-              <router-link :to="{ name: 'test', params: { id: test.id } }">
-                <v-btn variant="outlined" color="indigo-darken-3">
-                  Пройти
-                </v-btn>
-              </router-link>
-            </v-card-actions>
-          </v-card>
-        </v-col>
+          <v-card-actions>
+            <router-link :to="{ name: 'test', params: { id: test.id } }">
+              <v-btn variant="outlined" color="indigo-darken-3"> Пройти </v-btn>
+            </router-link>
+          </v-card-actions>
+        </v-card>
+      </v-col>
     </v-container>
   </div>
 
@@ -91,12 +111,17 @@
 </template>
 
 <script>
+import { testCategories } from '../sources/test_categories.js'
+
 export default {
   name: "MainComponent",
   data() {
     return {
       allTests: null,
       searchValue: null,
+      searchCategories: null,
+
+      testCategory: testCategories
     };
   },
   methods: {
@@ -110,8 +135,9 @@ export default {
       return { testsList };
     },
     findSearchedTest() {
-      this.allTests.newTestArray = this.allTests.testsList.filter(searchingTest => searchingTest.title == this.searchValue)
-      
+      this.allTests.newTestArray = this.allTests.testsList.filter(
+        (searchingTest) => searchingTest.title == this.searchValue || searchingTest.category == this.searchCategories
+      );
     },
   },
   async created() {
